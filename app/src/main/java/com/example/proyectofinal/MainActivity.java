7d1268eb80cd29843aeb8618861e41f9bc574d08
vanessarriagada <vanessarriagada@udec.cl>
2019-07-08 21:10:46 -0400

    Se añadio el mapa.

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.proyectofinal;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
